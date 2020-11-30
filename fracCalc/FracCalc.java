96c286e54a93cb417291fef7a954fa1e15a6fc4f
Joshua Silva <joshua.silvas1@stu.austinisd.org>
2020-11-30 14:21:29 +0000

    Completed Checkpoint One.

--- fracCalc/FracCalc.java
+++ fracCalc/FracCalc.java
@@ -1,11 +1,19 @@
 package fracCalc;
+import java.util.*;
 
 public class FracCalc {
 
     public static void main(String[] args) 
     {
         // TODO: Read the input from the user and call produceAnswer with an equation
-
+        String input = "";
+        Scanner console = new Scanner(System.in);
+        while(!(input.equals("quit"))){
+          System.out.print("Input: "); input = console.nextLine();
+          System.out.println(produceAnswer(input));
+          System.out.println();
+        }
+        console.close();
     }
     
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -19,8 +27,15 @@ public class FracCalc {
     public static String produceAnswer(String input)
     { 
         // TODO: Implement this function to produce the solution to the input
-        
-        return "";
+        if(input.equals("quit")){
+          return "You have exited the program!";
+        }
+
+        String[] input_array = input.split(" ");
+        String Frac1 = input_array[0];
+        String Operator = input_array[1];
+        String Frac2 = input_array[2];
+        return "Frac 1: " + Frac1 + " \nOperator: " + Operator + " \nFrac2 : " + Frac2;
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
